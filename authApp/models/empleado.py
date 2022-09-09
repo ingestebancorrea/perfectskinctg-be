73cfa27506ac089ba83def21d4f7df2dae66470b
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-09 12:19:34 -0500

    empleados.py en models configurado

---
+++ authApp/models/empleado.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .user import User
+
+class Empleado(models.Model):
+    empleado_codigo = models.AutoField(primary_key=True)
+    codigo_usuario = models.ForeignKey(User, related_name='fk_empleados_usuarios', on_delete=models.CASCADE)
+    cargo = models.CharField('Cargo', max_length=20)
+    nombre = models.CharField('Nombre', max_length=20)
+    apellidos = models.CharField('Apellidos', max_length=20)
+    tipoDocumento = models.CharField('Tipo Documento', max_length=20)
+    nroDocumento = models.CharField('Numero Documento', max_length=15, unique=True)
+    sexo = models.CharField('Sexo', max_length=10)
+    telefono = models.CharField('Telefono', max_length=15)
+    email = models.CharField('Email', max_length=20)
+    direccion = models.CharField('Direccion', max_length=40)
+    estado = models.CharField('Estado', max_length=10)
