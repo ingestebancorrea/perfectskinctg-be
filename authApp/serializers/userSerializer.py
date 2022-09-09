40acba8e5b8f556114cc28a3de7ceba094a83de4
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-09 17:28:12 -0500

    __init__.py modificado de views v3

--- authApp/serializers/userSerializer.py
+++ authApp/serializers/userSerializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from authApp.models import Account,User
+from authApp.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
