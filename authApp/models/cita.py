8e16e76297dc401ef55d42a3090818fdf5216e94
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-09 12:14:59 -0500

    cliente.py en models configurado

--- authApp/models/cita.py
+++ authApp/models/cita.py
@@ -3,8 +3,8 @@ from .cliente import Cliente
 from .empleado import Empleado
 
 class Cita(models.Model):
-    id = models.AutoField(primary_key=True)
-    fecha = models.Date()
+    cita_codigo = models.AutoField(primary_key=True)
+    fecha = models.DateField()
     hora = models.CharField('Hora', max_length=10)
     lugar_servicio = models.CharField('Hora', max_length=20)
     cliente = models.ForeignKey(Cliente, related_name='fk_citas_clientes', on_delete=models.CASCADE)
---
+++ authApp/models/cliente.py
@@ -0,0 +1,15 @@
+from django.db import models
+from .user import User
+
+class Cliente(models.Model):
+    cliente_codigo = models.AutoField(primary_key=True)
+    codigo_usuario = models.ForeignKey(User, related_name='fk_clientes_usuarios', on_delete=models.CASCADE)
+    nombre = models.CharField('Nombre', max_length=20)
+    apellidos = models.CharField('Apellidos', max_length=20)
+    tipoDocumento = models.CharField('Tipo Documento', max_length=20)
+    nroDocumento = models.CharField('Numero Documento', max_length=15, unique=True)
+    sexo = models.CharField('Sexo', max_length=10)
+    telefono = models.CharField('Telefono', max_length=15)
+    email = models.CharField('Email', max_length=20)
+    direccion = models.CharField('Direccion', max_length=40)
+    estado = models.CharField('Estado', max_length=10)
