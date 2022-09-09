a01a8e1712c3c8ddd862645e1bb2616d01895c50
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-09 12:22:56 -0500

    modulos cita.py cliente.py empleado.py registrados en la aplición

--- authApp/admin.py
+++ authApp/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from .models.user import User
-from .models.account import Account
 from .models.cita import Cita
-from .models.servicio import Servicio
+from .models.cliente import Cliente
 from .models.empleado import Empleado
 
 admin.site.register(User)
-admin.site.register(Account)
 admin.site.register(Cita)
-admin.site.register(Servicio)
+admin.site.register(Cliente)
 admin.site.register(Empleado)
