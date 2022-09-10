4c646f735f7d77af9432f7577caed1cb859c634a
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-09 21:54:44 -0500

    models y views borrados de authApp

--- authApp/models/cliente.py
+++ authApp/models/cliente.py
@@ -2,7 +2,7 @@ from django.db import models
 from .user import User
 
 class Cliente(models.Model):
-    id = models.AutoField(primary_key=True)
+    codigo_cliente = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, related_name='fk_clientes_usuarios', on_delete=models.CASCADE)
     nombre = models.CharField('Nombre', max_length=20)
     apellidos = models.CharField('Apellidos', max_length=20)
