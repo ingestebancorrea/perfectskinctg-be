b5142eb43a2d345b4c5aefdd800ce583f8a1cf9e
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-11 14:18:03 -0500

    modificando clienteSerializer.py

--- authApp/serializers/clienteSerializer.py
+++ authApp/serializers/clienteSerializer.py
@@ -15,8 +15,10 @@ class ClienteSerializer(serializers.ModelSerializer):
         return clienteInstance
     
     def to_representation(self, obj):
-        cliente = Cliente.objects.get(id=obj.id)
-        user = User.objects.get(cliente=obj.id)
+        user = User.objects.get(id=obj.id)
+        cliente = Cliente.objects.get(user=obj.id)
+        # cliente = Cliente.objects.get(id=obj.id)
+        # user = User.objects.get(cliente=obj.id)
         return {
             'id': cliente.id,
             'nombre': cliente.nombre,
