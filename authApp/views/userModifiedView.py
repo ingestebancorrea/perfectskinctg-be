59c89da2c36bdb1515e6baddca5e1f028cba3a7f
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-13 00:25:24 -0500

    haciendo migración de modelos

--- authApp/views/userModifiedView.py
+++ authApp/views/userModifiedView.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import views,status
+from rest_framework.permissions import IsAuthenticated
 from authApp.models.user import User
 from authApp.serializers.userSerializer import UserSerializer
 
@@ -7,6 +8,8 @@ class UserModifiedView(views.APIView):
     def put(request, pk=None):
         user = User.objects.filter(id = pk).first()
         user_serializer = UserSerializer(user, data = request.data)
+        permission_classes = (IsAuthenticated,)
+        
         if user_serializer.is_valid:
             user_serializer.save()
             return Response(user_serializer.data, status=status.HTTP_201_CREATED) 
