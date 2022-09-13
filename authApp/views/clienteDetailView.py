edfd59ba076c4f16ec548432160cdeade1927d6f
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-12 23:06:54 -0500

    configurando clienteDetailView.py

--- authApp/views/clienteDetailView.py
+++ authApp/views/clienteDetailView.py
@@ -1,6 +1,5 @@
 from django.conf import settings
-
-from rest_framework import status
+from rest_framework import generics
 from rest_framework.response import Response
 
 from authApp.models.cliente import Cliente
