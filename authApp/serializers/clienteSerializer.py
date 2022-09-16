606db7312e07bc0c6b190238bd88e1dad242d7cd
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-16 17:45:19 -0500

    Configurando clienteCreateView.py v2

--- authApp/serializers/clienteSerializer.py
+++ authApp/serializers/clienteSerializer.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from authApp.models import Cliente,User
-from .clienteSerializer import ClienteSerializer
+from .userSerializer import UserSerializer
 
 class ClienteSerializer(serializers.ModelSerializer):
-    cliente = ClienteSerializer()
+    user = UserSerializer()
     class Meta:
         model = Cliente
         fields = ['id','user']
