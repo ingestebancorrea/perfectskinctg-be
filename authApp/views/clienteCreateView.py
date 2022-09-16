45e51214ebb26295bc2bd25120a130a432e127fc
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-16 17:47:41 -0500

    Configurando clienteCreateView.py v3

--- authApp/views/clienteCreateView.py
+++ authApp/views/clienteCreateView.py
@@ -1,4 +1,4 @@
-from rest_framework import APIView, status
+from rest_framework.views import APIView, status
 from rest_framework.response import Response
 
 from authApp.serializers.clienteSerializer import ClienteSerializer
