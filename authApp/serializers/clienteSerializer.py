99014b8741dd251f1931a9b3810730977d571517
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-16 18:45:00 -0500

    Configurando clienteSerializer

--- authApp/serializers/clienteSerializer.py
+++ authApp/serializers/clienteSerializer.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
 from authApp.models import Cliente,User
-from .userSerializer import UserSerializer
 
 class ClienteSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
     class Meta:
         model = Cliente
         fields = ['id','user']
