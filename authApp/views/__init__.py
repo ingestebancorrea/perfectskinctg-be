0a6fc3c73624bba6dbdf3af5ca60f4e842619213
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-16 18:47:40 -0500

    Configurando __init__.py de views

--- authApp/views/__init__.py
+++ authApp/views/__init__.py
@@ -1,6 +1,2 @@
 from .userCreateView import UserCreateView
 from .userDetailView import UserDetailView
-from .userModifiedView import UserModifiedView
-from .userDeleteView import UserDeleteView
-from .clienteCreateView import ClienteCreateView
-from .clienteDetailView import ClienteDetailView
