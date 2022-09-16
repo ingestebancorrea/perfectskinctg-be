aab83443cc9bd13dab23c830d40fbc99e40d8283
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-16 18:43:05 -0500

    Configurando userSerializer

--- authApp/serializers/clienteSerializer.py
+++ authApp/serializers/clienteSerializer.py
@@ -7,35 +7,3 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = ['id','user']
-    
-    #recibe un JSON y lo convierte a objeto, **validated_data: manda argumentos en forma de diccionario
-    def create(self, validated_data):
-        userData = validated_data.pop('user') #extrae primera parte del JSON "user" con pop
-        clienteInstance = Cliente.objects.create(**validated_data) #enviar campos restantes
-        User.objects.create(cliente=clienteInstance,**userData) 
-        return clienteInstance
-    
-    #con los modelos y metodo get se convierte en un unico objeto
-    def to_representation(self, obj):
-        cliente = Cliente.objects.get(id=obj.id) 
-        user = User.objects.get(cliente=obj.id)
-        return { #return{"contiene objeto de tipo JSON"}
-            'id': cliente.id,
-            'user': {
-                'id': user.id,
-                'username': user.username,
-                'password': user.password,
-                'nombre': user.nombre,
-                'apellidos': user.apellidos,
-                'tipoDocumento': user.tipoDocumento,
-                'nroDocumento': user.nroDocumento,
-                'email': user.email,
-                'tipoUsuario': user.tipoUsuario
-            }
-        }
-        
-# La relación se controla en el clienteSerializer, 
-# campo user con ayuda de UserSerializer, permite manejar la relación entra ambas entidades.
-
-# Sobreescritura de métodos 
-# create (crea al cliente con su respectivo usuario simultaneo)
--- authApp/serializers/userSerializer.py
+++ authApp/serializers/userSerializer.py
@@ -1,27 +1,43 @@
-from authApp.models import User
+from authApp.models import User, Cliente
 from rest_framework import serializers
+from .clienteSerializer import ClienteSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+    cliente = ClienteSerializer()
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'nombre', 'apellidos', 'tipoDocumento', 'nroDocumento', 'email', 'tipoUsuario']  # fields [es un arreglo de cadenas]
+        fields = ['id', 'username', 'password', 'nombre', 'apellidos', 'tipoDocumento', 'nroDocumento', 'email', 'tipoUsuario','cliente']  # fields [es un arreglo de cadenas]
 
-    # def create(self, validated_data):
-    #     userInstance = User.objects.create(**validated_data)
-    #     return userInstance
+    #recibe un JSON y lo convierte a objeto, **validated_data: manda argumentos en forma de diccionario
+    def create(self, validated_data):
+        clienteData = validated_data.pop('cliente') #extrae primera parte del JSON "user" con pop
+        userInstance = User.objects.create(**validated_data) #enviar campos restantes
+        Cliente.objects.create(user=userInstance,**clienteData) 
+        return userInstance
     
-    # def to_representation(self, obj):
-    #     user = User.objects.get(id=obj.id)
-    #     return{
-    #         'user': {
-    #         'id': user.id,
-    #         'username': user.username,
-    #         'password': user.password,
-    #         'nombre': user.nombre,
-    #         'apellidos': user.apellidos,
-    #         'tipoDocumento': user.tipoDocumento,
-    #         'nroDocumento': user.nroDocumento,
-    #         'email': user.email,
-    #         'tipoUsuario': user.tipoUsuario
-    #         }
-    #     }
+    #con los modelos y metodo get se convierte en un unico objeto
+    def to_representation(self, obj):
+        user = User.objects.get(id=obj.id)
+        cliente = Cliente.objects.get(user=obj.id) 
+        return{ #return{"contiene objeto de tipo JSON"}
+             'user': {
+             'id': user.id,
+             'username': user.username,
+             'password': user.password,
+             'nombre': user.nombre,
+             'apellidos': user.apellidos,
+             'tipoDocumento': user.tipoDocumento,
+             'nroDocumento': user.nroDocumento,
+             'email': user.email,
+             'tipoUsuario': user.tipoUsuario,
+             'Cliente':{
+                 'id': cliente.id
+             }
+             }
+     }
+
+# La relación se controla en el userSerializer, 
+# campo cliente con ayuda de UserSerializer, permite manejar la relación entra ambas entidades.
+
+# Sobreescritura de métodos 
+# create (crea al cliente con su respectivo usuario simultaneo)
