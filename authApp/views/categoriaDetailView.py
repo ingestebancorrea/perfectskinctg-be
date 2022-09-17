2cf9887983f4783b6c21ab2e394b33504c8dd8b8
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-17 00:04:25 -0500

    Configurando categoriaDetailView.py v8

--- authApp/views/categoriaDetailView.py
+++ authApp/views/categoriaDetailView.py
@@ -39,7 +39,7 @@ class CategoriaDetailView(APIView):
         return response
 
     def delete(self, pk):
-        item = Categoria.objects.get(pk=pk)
+        item = self.objects.get(pk=pk)
         item.delete()
         
         return Response({
