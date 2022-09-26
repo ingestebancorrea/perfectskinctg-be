6fea1159b507aa149c0495b418268950965bc550
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2022-09-26 16:35:16 -0500

    Configurando settings.py

--- authProject/settings.py
+++ authProject/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
-    "http://127.0.0.1:9000",
+    "http://127.0.0.1:8000",
     "https://perfectskinctg-be.herokuapp.com/"
 ]
 
