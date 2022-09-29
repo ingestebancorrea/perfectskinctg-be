c77d6e4c2f686ec0e6f2bbab9d6d1762908a08f2
TatianaCaicedo <tatianacaicedo26@gmail.com>
2022-09-29 12:16:01 -0500

    Se modifica citas.py y citaView

--- authApp/models/cita.py
+++ authApp/models/cita.py
@@ -6,9 +6,9 @@ from .servicio import Servicio
 class Cita(models.Model):
     id = models.AutoField(primary_key=True)
     fecha = models.DateField('Fecha Cita', auto_now=False,auto_now_add=True)
-    hora = models.CharField('Hora', max_length=10)
+    hora = models.CharField('Hora', max_length=20)
     lugar = models.CharField('Hora', max_length=20)
     cliente = models.ForeignKey(Cliente, related_name='fk_citas_clientes', on_delete=models.CASCADE)
-    empleado = models.ForeignKey(Empleado, related_name='fk_citas_empleados', on_delete=models.CASCADE)
+    empleado = models.ForeignKey(Empleado, related_name='fk_citas_empleados', on_delete=models.CASCADE, null=True)
     servicio = models.ForeignKey(Servicio,related_name='fk_citas_servicios', on_delete=models.CASCADE)
-    estado = models.CharField('Estado', max_length=10)
+    estado = models.CharField('Estado', max_length=10, default="Agendada")
--- authApp/serializers/empleadoSerializer.py
+++ authApp/serializers/empleadoSerializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from authApp.models import Empleado
 
 class EmpleadoSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Empleado
         fields = ['id', 'cargo', 'user']
