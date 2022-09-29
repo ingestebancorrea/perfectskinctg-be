3827e2a4b0370b91246679948e3542e14821f6cc
TatianaCaicedo <tatianacaicedo26@gmail.com>
2022-09-29 12:28:02 -0500

    Se modifica lugar en el modelo de cita

--- authApp/models/cita.py
+++ authApp/models/cita.py
@@ -7,7 +7,7 @@ class Cita(models.Model):
     id = models.AutoField(primary_key=True)
     fecha = models.DateField('Fecha Cita', auto_now=False,auto_now_add=True)
     hora = models.CharField('Hora', max_length=20)
-    lugar = models.CharField('Hora', max_length=20)
+    lugar = models.CharField('Lugar', max_length=20)
     cliente = models.ForeignKey(Cliente, related_name='fk_citas_clientes', on_delete=models.CASCADE)
     empleado = models.ForeignKey(Empleado, related_name='fk_citas_empleados', on_delete=models.CASCADE, null=True)
     servicio = models.ForeignKey(Servicio,related_name='fk_citas_servicios', on_delete=models.CASCADE)
