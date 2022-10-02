c1b860b08275570975b625d8592b20395d2af756
estebanalejob <eb54429@gmail.com>
2022-10-02 14:33:02 -0500

    se organizo el codigo

--- authApp/admin.py
+++ authApp/admin.py
@@ -5,8 +5,12 @@ from .models.cliente import Cliente
 from .models.empleado import Empleado
 
 # Registrarlo dentro de admin:
+
 admin.site.register(User)
+
 #admin.site.register(Cita)
+
 admin.site.register(Cliente)
+
 #admin.site.register(Empleado)
 
